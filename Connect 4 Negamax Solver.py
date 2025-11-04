b0104d5d11ee3e65cbc5bada1863698d77b22eed
V615 <liuyingwen03@gmail.com>
2025-11-04 05:07:46 -0700

    Multiprocessing

--- Connect 4 Negamax Solver.py
+++ Connect 4 Negamax Solver.py
@@ -1,76 +1,86 @@
 import numpy as np
-import random
 import multiprocessing as mp
 import os
+from functools import lru_cache
 
 ZOBRIST_TABLE = None
+HASH_MASK = None
 TRANS_TABLE = None
 BASE_BOARD = None
 CONNECT_N = None
-TASK_QUEUE = None
-RESULT_QUEUE = None
 ROOT_ALPHA = None
 ROOT_BETA = None
 
 
-def init_pool(zobrist_table, trans_table, board_state, connect, task_queue, result_queue, alpha, beta):
-    global ZOBRIST_TABLE, TRANS_TABLE, BASE_BOARD, CONNECT_N, TASK_QUEUE, RESULT_QUEUE, ROOT_ALPHA, ROOT_BETA
-    ZOBRIST_TABLE = zobrist_table
+@lru_cache(maxsize=None)
+def _generate_zobrist(rows: int, cols: int):
+    rng = np.random.default_rng(42 + rows * 31 + cols)
+    table = rng.integers(0, 1 << 63, size=(rows, cols, 2), dtype=np.uint64)
+    mask = (1 << 63) - 1
+    return table, mask
+
+
+def init_pool(zobrist_table, hash_mask, trans_table, board_state, connect, alpha, beta):
+    global ZOBRIST_TABLE, HASH_MASK, TRANS_TABLE, BASE_BOARD, CONNECT_N, ROOT_ALPHA, ROOT_BETA
+    ZOBRIST_TABLE = np.asarray(zobrist_table, dtype=np.uint64)
+    HASH_MASK = int(hash_mask)
     TRANS_TABLE = trans_table
-    BASE_BOARD = board_state
+    BASE_BOARD = np.array(board_state, dtype=np.int8, copy=True)
     CONNECT_N = connect
-    TASK_QUEUE = task_queue
-    RESULT_QUEUE = result_queue
     ROOT_ALPHA = alpha
     ROOT_BETA = beta
 
-def worker_process_root():
-    while True:
-        task = TASK_QUEUE.get()
-        if task is None:
-            break
 
-        y, x = task
-        solver = Connect4Solver(
-            BASE_BOARD,
-            CONNECT_N,
-            trans_table=TRANS_TABLE,
-            zobrist_table=ZOBRIST_TABLE,
-        )
-        solver.make_move(y, x)
-        opp_score, _ = solver.negamax(-ROOT_BETA, -ROOT_ALPHA, last_move=(y, x))
-        score = -opp_score
-        RESULT_QUEUE.put((score, x))
+def evaluate_root(move):
+    y, x = move
+    solver = Connect4Solver(
+        BASE_BOARD,
+        CONNECT_N,
+        trans_table=TRANS_TABLE,
+        zobrist_table=ZOBRIST_TABLE,
+        hash_mask=HASH_MASK,
+    )
+    solver.make_move(y, x)
+    opp_score, _ = solver.negamax(-ROOT_BETA, -ROOT_ALPHA, last_move=(y, x))
+    return -opp_score, x
 
 class Connect4Solver:
-    def __init__(self, board: list, connect: int = 4, trans_table=None, zobrist_table=None):
-        self.board = np.array(board)
+    def __init__(self, board, connect: int = 4, trans_table=None, zobrist_table=None, hash_mask=None):
+        self.board = np.array(board, dtype=np.int8, copy=True)
         self.player = 1 + np.count_nonzero(self.board) % 2
         self.connect = connect
         self.trans_table = trans_table if trans_table is not None else {}
 
         self.rows, self.cols = self.board.shape
-        self.top_empty = [self.rows-1-np.argmax(self.board[::-1,c] == 0) for c in range(self.cols)]   # -1 if column is full
+        self.column_order = sorted(range(self.cols), key=lambda c: abs(c - self.cols // 2))
+        self.top_empty = []
+        for c in range(self.cols):
+            col = self.board[:, c]
+            empties = np.where(col == 0)[0]
+            self.top_empty.append(int(empties[-1]) if empties.size else -1)
 
-        self.init_hash(zobrist_table)
+        self.init_hash(zobrist_table, hash_mask)
 
-    def init_hash(self, zobrist_table):
-        if zobrist_table is None:
-            hash_bits = int(self.rows * self.cols * 1.585) + 1
-            random.seed(42)     # Tradition 
-            self.zobrist_table = [[[random.getrandbits(hash_bits) for _ in range(2)]    # two players
-                                   for _ in range(self.cols)]
-                                   for _ in range(self.rows)]
+    def init_hash(self, zobrist_table=None, hash_mask=None):
+        if zobrist_table is None or hash_mask is None:
+            table, mask = _generate_zobrist(self.rows, self.cols)
         else:
-            self.zobrist_table = zobrist_table
+            table, mask = zobrist_table, hash_mask
+
+        self.zobrist_table = np.asarray(table, dtype=np.uint64)
+        self.hash_mask = int(mask)
 
         # Compute initial hash and its vertical symmetry
         self.curr_hash = 0
         self.curr_symm_hash = 0
         for (r, c), val in np.ndenumerate(self.board):
             if val != 0:
-                self.curr_hash ^= self.zobrist_table[r][c][val - 1]
-                self.curr_symm_hash ^= self.zobrist_table[r][self.cols-1-c][val - 1]
+                piece = int(self.zobrist_table[r, c, val - 1])
+                sym_piece = int(self.zobrist_table[r, self.cols - 1 - c, val - 1])
+                self.curr_hash ^= piece
+                self.curr_symm_hash ^= sym_piece
+        self.curr_hash &= self.hash_mask
+        self.curr_symm_hash &= self.hash_mask
 
     def get_init_score(self):
         """
@@ -160,64 +170,38 @@ class Connect4Solver:
         Generate possible moves ordered by heuristic scores.
         Returns a list of (y, x) tuples for valid moves.
         """
-        if all(t == -1 for t in self.top_empty):
-            return []
-        
-        scores = np.zeros(len(self.top_empty), dtype=int)
-        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
-
-        def count_dir(y, x, dx, dy, player):
-            length = 0
-            ny, nx = y + dy, x + dx
-            while 0 <= ny < self.rows and 0 <= nx < self.cols and self.board[ny][nx] == player:
-                length += 1
-                ny += dy
-                nx += dx
-            return length
-
-        for x, y in enumerate(self.top_empty):
-            if y == -1:
-                scores[x] = -1
-                continue
-
-            score = 0
-            for dx, dy in directions:
-                for player in (1, 2):
-                    total = count_dir(y, x, dx, dy, player) + count_dir(y, x, -dx, -dy, player)
-                    if total == 0:
-                        continue
-                    score += min(total, self.connect) ** 2
-            scores[x] = score
-
-        order = np.argsort(-scores)
         moves = []
-        for i in order:
-            if self.top_empty[i] >= 0:  # valid column
-                moves.append((self.top_empty[i], i))  # (y, x)
-        
+        for x in self.column_order:
+            y = self.top_empty[x]
+            if y >= 0:
+                moves.append((y, x))
         return moves
     
     def make_move(self, y, x):
         """Make a move on the board."""
         self.board[y][x] = self.player
-        self.top_empty[x] -= 1
+        self.top_empty[x] = y - 1
 
         # Update hash
-        self.curr_hash ^= self.zobrist_table[y][x][self.player - 1]
-        self.curr_symm_hash ^= self.zobrist_table[y][self.cols-1-x][self.player-1]
+        self.curr_hash ^= int(self.zobrist_table[y, x, self.player - 1])
+        self.curr_hash &= self.hash_mask
+        self.curr_symm_hash ^= int(self.zobrist_table[y, self.cols - 1 - x, self.player - 1])
+        self.curr_symm_hash &= self.hash_mask
         
         self.player = 3 - self.player
 
     def undo_move(self, y, x):
         """Undo a move on the board."""
         self.board[y][x] = 0
-        self.top_empty[x] += 1
+        self.top_empty[x] = y
 
         self.player = 3 - self.player
 
         # Update hash
-        self.curr_hash ^= self.zobrist_table[y][x][self.player - 1]
-        self.curr_symm_hash ^= self.zobrist_table[y][self.cols-1-x][self.player-1]
+        self.curr_hash ^= int(self.zobrist_table[y, x, self.player - 1])
+        self.curr_hash &= self.hash_mask
+        self.curr_symm_hash ^= int(self.zobrist_table[y, self.cols - 1 - x, self.player - 1])
+        self.curr_symm_hash &= self.hash_mask
 
     def negamax(self, alpha=-1, beta=1, last_move=None):
         """
@@ -235,7 +219,7 @@ class Connect4Solver:
             self.trans_table[self.curr_symm_hash] = result
             return score, None
 
-        best_score = -1
+        best_score = -2
         best_col = None
         for move in self.get_moves():
             y, x = move
@@ -251,16 +235,18 @@ class Connect4Solver:
             # Update alpha-beta bounds
             alpha = max(alpha, score)
             if alpha >= beta or score == 1:
+                symm_col = None if best_col is None else self.cols - 1 - best_col
                 self.trans_table[hash] = (best_score, best_col)
-                self.trans_table[self.curr_symm_hash] = (best_score, self.cols-1-best_col)  # symmetric column
+                self.trans_table[self.curr_symm_hash] = (best_score, symm_col)
                 return best_score, best_col
 
-        self.trans_table[hash] = (best_score, None)
-        self.trans_table[self.curr_symm_hash] = (best_score, None)  
-        return best_score, None
+        symm_col = None if best_col is None else self.cols - 1 - best_col
+        self.trans_table[hash] = (best_score, best_col)
+        self.trans_table[self.curr_symm_hash] = (best_score, symm_col)
+        return best_score, best_col
 
-    def negamax_parallel(self, alpha=-1, beta=1, last_move=None, processes=mp.cpu_count()):
-        """Parallel Negamax that uses a task queue for dynamic root move scheduling."""
+    def negamax_parallel(self, alpha=-1, beta=1, last_move=None, processes=None):
+        """Parallel Negamax with shared transposition table and center-first move ordering."""
         hash_key = self.curr_hash
         if hash_key in self.trans_table:
             return self.trans_table[hash_key]
@@ -273,60 +259,46 @@ class Connect4Solver:
             return result
 
         moves = self.get_moves()
-        worker_count = min(len(moves), processes)
+        if not moves:
+            result = (0, None)
+            self.trans_table[hash_key] = result
+            self.trans_table[self.curr_symm_hash] = result
+            return result
+
+        available = mp.cpu_count() if processes is None else max(1, min(processes, mp.cpu_count()))
+        worker_count = min(len(moves), available)
         if worker_count <= 1:
             return self.negamax(alpha, beta, last_move)
 
-        task_queue = mp.Queue()
-        result_queue = mp.Queue()
-
-        for move in moves:
-            task_queue.put(move)
-        for _ in range(worker_count):
-            task_queue.put(None)
-
-        board_state = self.board.tolist()
-        zobrist_table = self.zobrist_table
+        board_state = self.board.copy()
+        zobrist_table = self.zobrist_table.copy()
 
         with mp.Pool(
             processes=worker_count,
             initializer=init_pool,
             initargs=(
                 zobrist_table,
+                self.hash_mask,
                 self.trans_table,
                 board_state,
                 self.connect,
-                task_queue,
-                result_queue,
                 alpha,
                 beta,
             ),
         ) as pool:
-            workers = [pool.apply_async(worker_process_root) for _ in range(worker_count)]
-
-            best_score = -1
+            best_score = -2
             best_col = None
-            for _ in range(len(moves)):
-                score, col = result_queue.get()
+            for score, col in pool.imap_unordered(evaluate_root, moves):
                 if score > best_score:
                     best_score = score
                     best_col = col
-
-            for worker in workers:
-                worker.get()
-
-        for q in (task_queue, result_queue):
-            try:
-                q.close()
-                q.join_thread()
-            except (AttributeError, OSError):
-                pass
 
         result = (best_score, best_col)
         symm_col = None if best_col is None else self.cols - 1 - best_col
         self.trans_table[hash_key] = result
         self.trans_table[self.curr_symm_hash] = (best_score, symm_col)
         return result
+
 
 def show_result(solver, score, move):
     if score == 1:
@@ -338,26 +310,32 @@ def show_result(solver, score, move):
     print(f"{solver.__class__.__name__} Result")
     print(f"Winner: {winner}, Best move for Player {solver.player}: {move}")
 
+
 if __name__ == "__main__":
     mp.freeze_support()
 
     board = [
-        [0, 1, 0, 1, 2, 0, 2],
-        [0, 2, 0, 1, 2, 0, 1],
-        [0, 1, 0, 2, 1, 1, 2],
-        [0, 2, 0, 1, 2, 2, 1],
-        [0, 1, 0, 2, 1, 1, 2],
-        [2, 1, 0, 1, 2, 2, 1]
+        [0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0],
+        [0, 1, 0, 2, 1, 1, 0],
+        [0, 2, 0, 1, 2, 2, 0],
+        [0, 1, 0, 2, 1, 1, 0],
+        [2, 1, 0, 1, 2, 2, 0]
     ]
     num_processes = 100
     connect = 4
-    
+
     max_processes = max(1, min(num_processes, mp.cpu_count()))
     if max_processes > 1:
         manager = mp.Manager()
-        solver = Connect4Solver(board, connect=connect, trans_table=manager.dict())
-        score, move = solver.negamax_parallel(processes=max_processes)
+        try:
+            trans_table = manager.dict()
+            solver = Connect4Solver(board, connect=connect, trans_table=trans_table)
+            score, move = solver.negamax_parallel(processes=max_processes)
+        finally:
+            manager.shutdown()
     else:
-        shared_trans = {}
-    
+        solver = Connect4Solver(board, connect=connect)
+        score, move = solver.negamax()
+
     show_result(solver, score, move)
